dc61bfa8995c3d0341d617d188c8455c7518e6df
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2023-07-31 21:41:36 -0400

    styling sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
   return (
@@ -13,9 +13,27 @@ const Sidebar = () => {
         <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
       </Link>
       <nav>
-        {/* <NavLink exact="true" activeclassname="active" to="/">
+        <NavLink exact="true" activeclassname="active" to="/">
           <FontAwesomeIcon icon={faHome} color="#4d4d4d" />
-        </NavLink> */}
+        </NavLink>
+
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4d" />
+        </NavLink>
+
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4d" />
+        </NavLink>
       </nav>
     </div>
   )

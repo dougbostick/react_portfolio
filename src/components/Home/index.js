130ea11a20aed95287ea62703fbed17265f2b5b2
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2023-08-02 13:47:37 -0400

    working on home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,24 @@
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+import './index.scss'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm <img src={LogoTitle} alt="developer" />
+          lobodan
+          <br />
+          web developer
+        </h1>
+        <h2>Frontend Developer / Javascript Expert</h2>
+      </div>
+      <Link to="/contact" className="flat-button">
+        CONTACT ME
+      </Link>
+    </div>
+  )
+}
+
+export default Home
